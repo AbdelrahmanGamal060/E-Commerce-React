faf53bad005feb1939f5da8e62727a2b44c470f4
[200~login-Assignment~ <AbdelrahmanGamal060@gmail.com>
2025-07-25 21:20:50 +0300

    fix

--- src/components/protectedRoute/protectedRoute.jsx
+++ src/components/protectedRoute/protectedRoute.jsx
@@ -1,5 +1,5 @@
 import React, { } from 'react'
-import Style from "./ProtectedRoute.module.css"
+import Style from "./protectedRoute.module.css"
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedRoute({ children  }) {
